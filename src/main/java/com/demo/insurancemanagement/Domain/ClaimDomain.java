4a29824f206521ae6c8fba514923816655c077bc
=Ayushi Saxena <saxenaayu2106@gmail.com>
2023-03-31 16:10:03 +0530

    Adding ClientDomain

---
+++ src/main/java/com/demo/insurancemanagement/Domain/ClaimDomain.java
@@ -0,0 +1,5 @@
+package com.demo.insurancemanagement.Domain;
+
+public class ClaimDomain {
+
+}
---
+++ src/main/java/com/demo/insurancemanagement/Domain/ClientDomain.java
@@ -0,0 +1,42 @@
+package com.demo.insurancemanagement.Domain;
+
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "client_user", schema = "insuranceSystem")
+public class ClientDomain {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "DOB")
+	private LocalDate dateofBirth;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "phone")
+	private String phoneNumber;
+	
+	
+//	@OneToMany(mappedBy = "client")
+//	private List<InsurancePolicy> insurancePolicies;
+	
+}
